141628da06d1fabc489d272f3c55d6307c79d3c7
profxj <xavier@ucolick.org>
2023-11-06 17:08:55 -0800

    wip

---
+++ ihop/inference/mcmc.py
@@ -0,0 +1,79 @@
+""" MCMC module for IHOP """
+
+import os
+from importlib import resources
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import emcee
+import corner
+
+import torch
+
+from oceancolor.ihop import io as ihop_io
+
+
+def log_prob(ab, Rs, model, device, scl_sig):
+    pred = model.prediction(ab, device)
+    #
+    sig = scl_sig * Rs
+    #
+    return -1*0.5 * np.sum( (pred-Rs)**2 / sig**2)
+
+
+def run_emcee_nn(nn_model, Rs, nwalkers:int=32, nsteps:int=20000,
+                 save_file:str=None, p0=None, scl_sig:float=0.05):
+
+    # Device for NN
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Init
+    ndim = nn_model.ninput
+    if p0 is None:
+        p0 = np.random.rand(nwalkers, ndim)
+
+    # Set up the backend
+    # Don't forget to clear it in case the file already exists
+    if save_file is not None:
+        backend = emcee.backends.HDFBackend(save_file)
+        backend.reset(nwalkers, ndim)
+    else:
+        backend = None
+
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, 
+                                    args=[Rs, nn_model, device, scl_sig],
+                                    backend=backend)
+
+    # Burn in
+    print("Running burn-in")
+    state = sampler.run_mcmc(p0, 1000)
+    sampler.reset()
+
+    # Run
+    print("Running full model")
+    sampler.run_mcmc(state, nsteps)
+
+    print(f"All done: Wrote {save_file}")
+
+    # Return
+    return sampler
+
+if __name__ == '__main__':
+
+    # Load Hydrolight
+    print("Loading Hydrolight data")
+    ab, Rs, d_l23 = ihop_io.load_loisel_2023_pca()
+
+    # Load model
+    model_file = os.path.join(resources.files('oceancolor'), 
+                              'ihop', 'model_20000.pth')
+    print(f"Loading model: {model_file}")
+    model = torch.load(model_file)
+
+    # idx=200
+    idx = 200
+    save_file = f'MCMC_NN_i{idx}.h5'
+
+    run_emcee_nn(model, Rs[idx], save_file=save_file)
---
+++ ihop/iops/pca.py
@@ -0,0 +1,87 @@
+""" PCA-based parameterization of IOPs """
+
+import os
+import numpy as np
+
+from importlib import resources
+
+from ihop.hydrolight import loisel23
+
+pca_path = os.path.join(resources.files('ihop'),
+                            'data', 'PCA')
+
+
+def load_loisel_2023_pca():
+
+    # Load up data
+    l23_path = os.path.join(resources.files('ihop'),
+                            'data', 'PCA')
+    l23_a_file = os.path.join(l23_path, 'pca_L23_X4Y0_a_N3.npz')
+    l23_bb_file = os.path.join(l23_path, 'pca_L23_X4Y0_bb_N3.npz')
+
+    d_a = np.load(l23_a_file)
+    d_bb = np.load(l23_bb_file)
+    nparam = d_a['Y'].shape[1]+d_bb['Y'].shape[1]
+    ab = np.zeros((d_a['Y'].shape[0], nparam))
+    ab[:,0:d_a['Y'].shape[1]] = d_a['Y']
+    ab[:,d_a['Y'].shape[1]:] = d_bb['Y']
+
+    Rs = d_a['Rs']
+
+    # Return
+    return ab, Rs, d_a, d_bb
+
+
+#def generate_all_pca(clobber:bool=False):
+#    generate_l23_pca(clobber=clobber)
+#    generate_l23_tara_pca(clobber=clobber)
+
+
+def generate_l23_pca(clobber:bool=False, Ncomp:int=3):
+    """ Generate PCA models for IOPs in Loisel 2023 data
+
+    Args:
+        clobber (bool, optional): _description_. Defaults to False.
+        Ncomp (int, optional): _description_. Defaults to 3.
+    """
+
+    # Load up the data
+    X=4
+    Y=0
+    ds = loisel23.load_ds(X, Y)
+
+    # Loop on IOPs
+    for iop in ['a', 'b', 'bb']:
+        outfile = os.path.join(pca_path, f'pca_L23_X{X}Y{Y}_{iop}_N{Ncomp}.npz')
+        if not os.path.exists(outfile) or clobber:
+            pca.fit_normal(ds[iop].data, Ncomp, save_outputs=outfile,
+                           extra_arrays={'Rs':ds.Rrs.data,
+                                         'wavelength':ds.Lambda.data})
+
+    # L23 positive, definite
+
+def generate_l23_tara_pca(clobber:bool=False, return_N:int=None):
+
+    # Load up
+    wave_grid, tara_a_water, l23_a = load_tara()
+
+    # N components
+    data = np.append(l23_a, tara_a_water, axis=0)
+    for N in [3,5,20]:
+        outfile = os.path.join(pca_path, f'pca_L23_X4Y0_Tara_a_N{N}.npz')
+        if not os.path.exists(outfile) or clobber or ( (return_N is not None) 
+                                                      and (return_N == N) ):
+            print(f"Fit PCA with N={N}")
+            pca_fit = pca.fit_normal(data, N, save_outputs=outfile,
+                           extra_arrays={'wavelength':wave_grid})
+            if return_N is not None and return_N == N:
+                return data, wave_grid, pca_fit
+
+def reconstruct(Y, pca_dict, idx):
+    # Grab the orginal
+    orig = pca_dict['data'][idx]
+
+    # Reconstruct
+    recon = np.dot(Y, pca_dict['M']) + pca_dict['mean']
+
+    return orig, recon 
